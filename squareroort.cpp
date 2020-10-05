32dca62add07987990223c78d36d4d4df8861c84
Rahul Ranjan <43896338+rahulbits@users.noreply.github.com>
2020-10-05 11:29:17 +0530

    Update squareroort.cpp

--- squareroort.cpp
+++ squareroort.cpp
@@ -1,3 +1,7 @@
+// Square root of an integer
+//Given an integer x, find it’s square root. If x is not a perfect square, then return floor(√x).
+// Using Binary Search Algo
+
 # include<bits/stdc++.h>
 using namespace std;
 int  sqrt(int x)
@@ -11,20 +15,18 @@ int  sqrt(int x)
 			mid = (l+end)/2;
 			if(mid*mid == x)
 			return mid;
-			else if(mid*mid < x){
-			
+			else if(mid*mid < x)
+			{
 			l = mid+1;
-			ans = mid; }
+			ans = mid; 
+			}
 			else
 			end = mid-1;
-			
 		}
 		return ans;
-	}
-}
+	      }
+ }
 int main(){
 	int x ; cin>>x;
-	cout<<sqrt(x)<<endl;
-	
-	
+	cout<<sqrt(x)<<endl;	
 }
